a1fcd783e461b93dff13eae64198b08fb55336dc
Niels Nübel <n.nuebel@nn-medienagentur.de>
2018-04-05 19:24:28 +0200

    Add ready Class

--- tmpl/default.php
+++ tmpl/default.php
@@ -17,7 +17,7 @@ $root = 'kickslider';
     <ul class="kickslider">
 <?php foreach ($slides as $slide): ?>
         <li class="kickslider__item">
-            <div class="kickslider__background" style="background-image: url(<?= $slide->image ?>); <?= $slide->position ?>"></div>
+            <div class="kickslider__background" style="background-image: url(<?= $slide->image ?>)"></div>
             <?= JHtml::_('image', $slide->image, htmlspecialchars($slide->alt, ENT_COMPAT, 'UTF-8'), $slide->attr) ?>
             <div class="kickslider__item_content">
                 <div>
@@ -48,6 +48,7 @@ $root = 'kickslider';
             var slider = document.querySelectorAll('.<?= $root . '_' . $module->id ?> .kickslider');
 
             slider[0].style.height = biggestElement + 'px';
+            slider[0].className += " ready";
         }
 
 
@@ -68,7 +69,6 @@ $root = 'kickslider';
             setTimeout(showSlides, <?= $duration ?>);
         }
     }
-window.onload = function() {
+
     initSlider<?= $module->id ?>()
-};
 </script>
